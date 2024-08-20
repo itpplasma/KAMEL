c29e063ca6fae955d3eee5f7d30b1ca50ffa7fac
Markl, Markus <markl@tugraz.at>
2024-08-20 10:46:38 +0200

    Really repair f2py_Ifuncs.sh.

--- python/susc_functions/f2py_Ifuncs.sh
+++ python/susc_functions/f2py_Ifuncs.sh
@@ -1,12 +1,16 @@
-f2py -c src/getIfunc.f90 src/W2_arr.f90 ../../KiLCA/flre/conductivity/calc_Imn_array.f90 -m susc_funcs \
+curr_loc=$PWD
+libs_path=$curr_loc/../../../libs
+lib_kilca=$curr_loc/../../KiLCA/lib/
+f2py -c src/getIfunc.f90 src/W2_arr.f90 $curr_loc/../../KiLCA/flre/conductivity/calc_Imn_array.f90 -m susc_funcs \
 	-llapack \
 	-lblas \
-	-lkilca  \
-	-L../../../libs/sundials/build/src/cvode/ -lsundials_cvode \
-	-L../../../libs/sundials/build/src/nvector/serial/ -lsundials_nvecserial \
-	-L../../../libs/slatec/ -lslatec \
-	-L../../../bessel/lib/ -lbessel \
+	-L$lib_kilca -lkilca  \
+	-L$libs_path/sundials/build/src/cvode/ -lsundials_cvode \
+	-L$libs_path/sundials/build/src/nvector/serial/ -lsundials_nvecserial \
+	-L$libs_path/slatec/ -lslatec \
+	-L$libs_path/bessel/lib/ -lbessel \
 	-lgsl \
 	-lgslcblas \
 	-lm \
-	-lc
+	-lc \
+	--verbose

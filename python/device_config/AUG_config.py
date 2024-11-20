dc561d4c6774a4bad38cbd606c60d8db795b71a4
Markus Markl <markl@tugraz.at>
2024-11-20 08:34:39 +0100

    Add AUG config python class and extend MASTU config class.

---
+++ python/device_config/AUG_config.py
@@ -0,0 +1,11 @@
+class AUG_config:
+    
+    R0 = 165.0
+    r_eff_wall = 80.0 # ideal wall effective radius
+    r_eff_antenna = 70.0 # antenna effective radius
+    r_eff_plas = 67.0
+    Btor = -17563.3704 # shot dependent, but gives order of magnitude
+    I0_rmp = 5.1e12
+
+    def __init__(self):
+        pass
--- python/device_config/MASTU_config.py
+++ python/device_config/MASTU_config.py
@@ -3,6 +3,9 @@ class MASTU_config:
     R0 = 80.0
     r_eff_wall = 95.0 # ideal wall effective radius
     r_eff_antenna = 93.0 # antenna effective radius
+    r_eff_plasma = 85.0 # approximate effective plasma radius determined by separatrix
+    Btor = 6550.0 # approximate value to give order of magnitudeo
+    I0_rmp = 6.0e12
 
     def __init__(self):
         pass

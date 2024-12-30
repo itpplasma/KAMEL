f82b01094a53e8108c9f024eaef1c56abe37fea1
Markus Markl <markl@tugraz.at>
2024-12-30 08:31:52 +0100

    Add KiLCA setup script and cleanup CMakeLists

---
+++ KiLCA/kilca.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+mkdir external
+cd external
+echo "Building Slatec..."
+mkdir slatec
+cd slatec
+mkdir lib
+curl -L https://www.netlib.org/slatec/slatec_src.tgz -o - | tar xz
+
+# Check the operating system
+if [[ "$(uname)" == "Darwin" ]]; then
+    # macOS specific compiler flags
+    CFLAGS="-O2"
+elif [[ "$(uname)" == "Linux" ]]; then
+    # Linux specific compiler flags
+    CFLAGS="-msse2 -mfpmath=sse"
+else
+    # Default compiler flags for other operating systems
+    CFLAGS=""
+fi
+gfortran -c -Wall -Wtabs -mtune=generic $CFLAGS src/*.f
+ar rcs libslatec.a *.o
+rm *.o
+mv libslatec.a lib/
+echo "Finished building Slatec..."
+
+cd ../ # external
+echo ""
+
+#echo "Building Bessel..."
+#mkdir bessel
+#cd bessel
+#cp -r ../../KiLCA/math/bessel/* .
+#cd slatec
+#gfortran -c -Wall -Wtabs -mtune=generic $CFLAGS *.f
+#ar rcs libbessel.a *.o
+#rm *.o
+#mv libbessel.a ../../lib/
+#cd ../../
+#echo "Finished building Bessel..."
+#echo ""
+
+echo "Building lapack..."
+curl -L http://www.netlib.org/lapack/lapack-3.2.1.tgz -o - | tar xz
+cd lapack-3.2.1
+cp INSTALL/make.inc.gfortran make.inc
+make -j lib
+make -j blaslib
+make clean
+cd ..
+echo "Finished building lapack..."
+echo ""
+
+echo "Building gsl-2.4"
+curl -L https://ftp.gnu.org/gnu/gsl/gsl-2.4.tar.gz -o - | tar xz
+cd gsl-2.4
+./configure
+make -j
+cd ..
+echo "Finished building gsl-2.4..."
+echo ""
+
+#echo "Building SuperLU..."
+#wget https://portal.nersc.gov/project/sparse/superlu/superlu_4.1.tar.gz
+#tar -xzvf superlu_4.1.tar.gz
+#rm superlu_4.1.tar.gz
+#cd SuperLU_4.1
+#cp MAKE_INC/make.linux make.inc
+## Check the operating system
+#if [[ "$(uname)" == "Darwin" ]]; then
+    ## macOS specific compiler flags
+    #CFLAGS="-O2"
+    #CFLAGS+=" -I/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include"
+    #sed -E "s|SuperLUroot\t=.*|SuperLUroot\t= $(pwd)|g" make.inc >> make.inc.tmp
+    #mv make.inc.tmp make.inc
+    #sed -E "s|BLASLIB[[:space:]]*=.*|BLASLIB = #-lblas|g" make.inc >> make.inc.tmp
+    #mv make.inc.tmp make.inc
+    #sed -E "s|RANLIB       = ranlib|RANLIB       = gcc-ranlib-12|g" make.inc >> make.inc.tmp
+    #mv make.inc.tmp make.inc
+    #sed -E "s|CC           = gcc|CC           = gcc-12|g" make.inc >> make.inc.tmp
+    #mv make.inc.tmp make.inc
+    #sed -E "s|ARCH         = ar|ARCH         = gcc-ar-12|g" make.inc >> make.inc.tmp
+    #mv make.inc.tmp make.inc
+    #sed -E "s|CFLAGS       = -O3|CFLAGS       = -O3 -I/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include|g" make.inc >> make.inc.tmp
+    #mv make.inc.tmp make.inc
+#elif [[ "$(uname)" == "Linux" ]]; then
+    ## Linux specific compiler flags
+    #sed -i "s|SuperLUroot\t= \$(HOME)/Codes/SuperLU_4.1|SuperLUroot = $(pwd)|" make.inc
+    #sed -i 's/g77/gfortran/g' make.inc
+#fi
+#make -j
+#echo "Finished building SuperLU..."
+#echo ""
+#cd ..
+#cd ..
+
+
+cd ../KiLCA
+mkdir build
+cd build
+cmake ..
+make -j

ce7a46e3af04add7e953a7e3081dcbe262a13242
zandivx <80193776+zandivx@users.noreply.github.com>
2025-01-03 00:08:08 +0100

    Improve kamel.sh
    Ensure that script executes in the right directory and check for errors

--- kamel.sh
+++ kamel.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-./KiLCA/kilca.sh
-./KIM/kim.sh
-cd QL-Balance
-./ql-balance.sh
+
+OLD_DIR=$(pwd)
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd $SCRIPT_DIR
+./KiLCA/kilca.sh || exit $?
+./KIM/kim.sh || exit $?
+./QL-Balance/ql-balance.sh || exit $?
+cd $OLD_DIR

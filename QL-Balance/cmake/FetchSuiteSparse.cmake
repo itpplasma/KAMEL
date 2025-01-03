47a3a808da0e47d2483e4a06ad324dac067c6407
Christopher Albert <albert@tugraz.at>
2025-01-03 22:25:54 +0100

    Add static suffix and fPIC

--- QL-Balance/cmake/FetchSuiteSparse.cmake
+++ QL-Balance/cmake/FetchSuiteSparse.cmake
@@ -6,23 +6,29 @@ ExternalProject_Add(
     DOWNLOAD_EXTRACT_TIMESTAMP
     URL https://github.com/DrTimothyAldenDavis/SuiteSparse/archive/refs/tags/v7.8.3.tar.gz
     URL_HASH MD5=242e38ecfc8a3e3aa6b7d8d44849c5cf
-    CMAKE_ARGS -GNinja -DUMFPACK_USE_CHOLMOD=OFF -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/install
+    CMAKE_ARGS
+        -GNinja
+        -DUMFPACK_USE_CHOLMOD=OFF
+        -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/install
+        -DCMAKE_C_FLAGS=-fPIC
+        -DCMAKE_CXX_FLAGS=-fPIC
+        -DCMAKE_Fortran_FLAGS=-fPIC
     BUILD_COMMAND ${CMAKE_COMMAND} --build <BINARY_DIR> --target UMFPACK/install
                 && ${CMAKE_COMMAND} --build <BINARY_DIR> --target AMD/install
                 && ${CMAKE_COMMAND} --build <BINARY_DIR> --target SuiteSparse_config/install
     INSTALL_COMMAND ""
     BUILD_BYPRODUCTS
-        ${CMAKE_BINARY_DIR}/install/lib/libumfpack.a
-        ${CMAKE_BINARY_DIR}/install/lib/libsuitesparseconfig.a
-        ${CMAKE_BINARY_DIR}/install/lib/libamd.a
+        ${CMAKE_BINARY_DIR}/install/lib/libumfpack${CMAKE_STATIC_LIBRARY_SUFFIX}
+        ${CMAKE_BINARY_DIR}/install/lib/libsuitesparseconfig${CMAKE_STATIC_LIBRARY_SUFFIX}
+        ${CMAKE_BINARY_DIR}/install/lib/libamd${CMAKE_STATIC_LIBRARY_SUFFIX}
         ${CMAKE_BINARY_DIR}/install/include/suitesparse/umfpack.h
         <SOURCE_DIR>UMFPACK/Demo/umf4_f77wrapper.c
         <SOURCE_DIR>UMFPACK/Demo/umf4_f77zwrapper.c
 )
 
-set(UMFPACK_LIBRARY_PATH ${CMAKE_BINARY_DIR}/install/lib/libumfpack.a)
-set(SUITESPARSE_CONFIG_LIBRARY_PATH ${CMAKE_BINARY_DIR}/install/lib/libsuitesparseconfig.a)
-set(AMD_LIBRARY_PATH ${CMAKE_BINARY_DIR}/install/lib/libamd.a)
+set(UMFPACK_LIBRARY_PATH ${CMAKE_BINARY_DIR}/install/lib/libumfpack${CMAKE_STATIC_LIBRARY_SUFFIX})
+set(SUITESPARSE_CONFIG_LIBRARY_PATH ${CMAKE_BINARY_DIR}/install/lib/libsuitesparseconfig${CMAKE_STATIC_LIBRARY_SUFFIX})
+set(AMD_LIBRARY_PATH ${CMAKE_BINARY_DIR}/install/lib/libamd${CMAKE_STATIC_LIBRARY_SUFFIX})
 set(UMFPACK_INCLUDE_DIR ${CMAKE_BINARY_DIR}/install/include/suitesparse)
 
 ExternalProject_Get_Property(SuiteSparse SOURCE_DIR)

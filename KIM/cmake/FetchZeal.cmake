94ac6f1523885efa4e12d37816bc9de572a5ccf2
Markus Markl <markl@tugraz.at>
2025-01-06 19:27:04 +0100

    Initiate KIM Makefile

---
+++ KIM/cmake/FetchZeal.cmake
@@ -0,0 +1,17 @@
+include(ExternalProject)
+
+ExternalProject_Add(
+    Zeal
+    DOWNLOAD_EXTRACT_TIMESTAMP
+    URL https://elsevier.digitalcommonsdata.com/public-files/datasets/yc9vv7rwyj/files/be55b15b-6d9c-4f0e-b5f7-09b300cfb806/file_downloaded
+    CMAKE_ARGS -GNinja
+    INSTALL_COMMAND ""
+    BUILD_BYPRODUCTS
+        ${CMAKE_BINARY_DIR}/install/lib/libzeal${CMAKE_STATIC_LIBRARY_SUFFIX}
+        ${CMAKE_BINARY_DIR}/install/lib/libzeal${CMAKE_SHARED_LIBRARY_SUFFIX}
+)
+
+add_library(Zeal::zeal SHARED IMPORTED)
+set_target_properties(Zeal::zeal PROPERTIES
+    IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/install/lib/libzeal${CMAKE_SHARED_LIBRARY_SUFFIX}
+)

7b670afbf8c232ec6f958579184c76e56fee1e0a
Markl, Markus <markl@tugraz.at>
2025-01-23 14:56:18 +0100

    Fix Zeal build in kim

--- KIM/cmake/FetchZeal.cmake
+++ KIM/cmake/FetchZeal.cmake
@@ -4,8 +4,11 @@ ExternalProject_Add(
     Zeal
     DOWNLOAD_EXTRACT_TIMESTAMP
     URL https://elsevier.digitalcommonsdata.com/public-files/datasets/yc9vv7rwyj/files/be55b15b-6d9c-4f0e-b5f7-09b300cfb806/file_downloaded
-    CMAKE_ARGS -GNinja
+    #CMAKE_ARGS -GNinja
     INSTALL_COMMAND ""
+    BUILD_COMMAND make F90=gfortran LIBS=-llapack
+    CONFIGURE_COMMAND ""
+    BUILD_IN_SOURCE TRUE
     BUILD_BYPRODUCTS
         ${CMAKE_BINARY_DIR}/install/lib/libzeal${CMAKE_STATIC_LIBRARY_SUFFIX}
         ${CMAKE_BINARY_DIR}/install/lib/libzeal${CMAKE_SHARED_LIBRARY_SUFFIX}

5f847be64bf76de3a3b3c483e4450e16fdec5153
Markl, Markus <markl@tugraz.at>
2025-01-23 15:06:20 +0100

    Remove Zeal build

--- KIM/cmake/FetchZeal.cmake
+++ KIM/cmake/FetchZeal.cmake
@@ -6,15 +6,14 @@ ExternalProject_Add(
     URL https://elsevier.digitalcommonsdata.com/public-files/datasets/yc9vv7rwyj/files/be55b15b-6d9c-4f0e-b5f7-09b300cfb806/file_downloaded
     #CMAKE_ARGS -GNinja
     INSTALL_COMMAND ""
-    BUILD_COMMAND make F90=gfortran LIBS=-llapack
+    BUILD_COMMAND ""#make F90=gfortran LIBS=-llapack
     CONFIGURE_COMMAND ""
-    BUILD_IN_SOURCE TRUE
     BUILD_BYPRODUCTS
         ${CMAKE_BINARY_DIR}/install/lib/libzeal${CMAKE_STATIC_LIBRARY_SUFFIX}
         ${CMAKE_BINARY_DIR}/install/lib/libzeal${CMAKE_SHARED_LIBRARY_SUFFIX}
 )
 
-add_library(Zeal::zeal SHARED IMPORTED)
-set_target_properties(Zeal::zeal PROPERTIES
-    IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/install/lib/libzeal${CMAKE_SHARED_LIBRARY_SUFFIX}
-)
+#add_library(Zeal::zeal SHARED IMPORTED)
+#set_target_properties(Zeal::zeal PROPERTIES
+#    IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/install/lib/libzeal${CMAKE_SHARED_LIBRARY_SUFFIX}
+#)

c4780c29b6f22fd3f7e7b5ff02dc5cdc9bc417ee
Markus Markl <markl@tugraz.at>
2025-02-20 08:03:43 +0100

    Add test of D one over nu to write out data

--- QL-Balance/src/full/D_one_over_nu.f90
+++ QL-Balance/src/full/D_one_over_nu.f90
@@ -8,7 +8,7 @@ subroutine get_D_one_over_nu
     use libneo_kinds, only: real_kind
 
     use grid_mod, only: Donue11, Donue12, Donue21, Donue22, &
-        Donui11, Donui12, Donui21, Donui22, npoib
+        Donui11, Donui12, Donui21, Donui22, npoic, rb
     
     use plasma_parameters, only: params
     use baseparam_mod, only: c, Btor, Rtor, ev, e_charge
@@ -22,8 +22,8 @@ subroutine get_D_one_over_nu
     real(kind=real_kind) :: diff_coeff
 
     if (.not. allocated(Donue11)) then
-        allocate(Donue11(npoib), Donue12(npoib), Donue21(npoib), Donue22(npoib))
-        allocate(Donui11(npoib), Donui12(npoib), Donui21(npoib), Donui22(npoib))
+        allocate(Donue11(npoic), Donue12(npoic), Donue21(npoic), Donue22(npoic))
+        allocate(Donui11(npoic), Donui12(npoic), Donui21(npoic), Donui22(npoic))
     end if
 
     call init_gauss_laguerre_integration(5.0d0/2.0d0)
@@ -36,32 +36,34 @@ subroutine get_D_one_over_nu
     w92 = w
     x92 = x
 
-    do ipoi=1, npoib
+    do ipoi=1, npoic
 
         ! set up species, (Te, Ti, ne)
-        call init_deuterium_plasma(params(3,ipoi), params(4,ipoi), params(1,ipoi), species_array)
-
+        call init_deuterium_plasma(params(3,ipoi)/ev, params(4,ipoi)/ev, params(1,ipoi), species_array)
         do i = 1, num_species
             species_array(i)%rho_L = species_array(i)%mass * c * &
-                sqrt(species_array(i)%temp * ev / species_array(i)%mass)/ e_charge / Btor
+                sqrt(species_array(i)%temp * ev / species_array(i)%mass)/ e_charge / abs(Btor)
         end do
-        
+
+        !print *, "ipoi = ", ipoi,  " Te = ", species_array(1)%temp, "Ti = ", species_array(2)%temp
         call fill_species_arr_coulomb_log(2, species_array)
+
         
         w = w52
         x = x52
         call calc_D_one_over_nu(1, 2, species_array, Rtor, diff_coeff)
         Donue11(ipoi) = diff_coeff
+        print *, "ipoi = ", ipoi, " Donue11 = ", diff_coeff
         call calc_D_one_over_nu(2, 2, species_array, Rtor, diff_coeff)
         Donui11(ipoi) = diff_coeff
 
         w = w72
         x = x72
         call calc_D_one_over_nu(1, 2, species_array, Rtor, diff_coeff)
-        Donue21(ipoi) = diff_coeff
+        Donue12(ipoi) = diff_coeff
         Donue21(ipoi) = diff_coeff
         call calc_D_one_over_nu(2, 2, species_array, Rtor, diff_coeff)
-        Donui21(ipoi) = diff_coeff
+        Donui12(ipoi) = diff_coeff
         Donui21(ipoi) = diff_coeff
 
         w = w92
@@ -72,5 +74,14 @@ subroutine get_D_one_over_nu
         Donui22(ipoi) = diff_coeff
 
     end do
+
+    print *, "D_one_over_nu calculated"
+
+    open(10, file='D_one_over_nu.dat')
+    do ipoi=1, npoic
+        write(10,*) rb(ipoi), Donue11(ipoi), Donue12(ipoi), Donue21(ipoi), Donue22(ipoi), &
+            Donui11(ipoi), Donui12(ipoi), Donui21(ipoi), Donui22(ipoi)
+    end do
+    close(10)
 
 end subroutine
--- QL-Balance/src/test/test_D_one_over_nu.f90
+++ QL-Balance/src/test/test_D_one_over_nu.f90
@@ -1,8 +1,20 @@
 program test_D_one_over_nu
 
-    implicit none
+    use balanceBase, only: balance_t
+    use balance_mod
+    use parallelTools
 
+    implicit none
+    class(balance_t), allocatable :: balance_instance
+    character(100) :: typ_of_run = "SingleStep"
+
+    call read_config
+    call from_balance_factory_get_balance(typ_of_run, balance_instance)
+    call initMPI
+    call balance_instance%init_balance()
 
     call get_D_one_over_nu
+
+    call MPI_finalize(ierror)
 
 end program

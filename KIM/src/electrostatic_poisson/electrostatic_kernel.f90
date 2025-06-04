e526f238f336614ca12d4ec23e6875180e9d7b7f
Markus Markl <markl@tugraz.at>
2025-06-04 15:41:04 +0200

    Start implementation of Fokker-Planck collisions

--- KIM/src/electrostatic_poisson/electrostatic_kernel.f90
+++ KIM/src/electrostatic_poisson/electrostatic_kernel.f90
@@ -136,7 +136,7 @@ module electrostatic_kernel
             end do
         end do
 
-        kernel_phi_llp = kernel_phi_llp / (8.0d0 * pi**3.0d0)  /2.0d0 ! factor 1/2 is somehow missing. Including this factor nicely reproduces the debye case.
+        kernel_phi_llp = kernel_phi_llp / (8.0d0 * pi**3.0d0) ! /2.0d0 ! factor 1/2 is somehow missing. Including this factor nicely reproduces the debye case.
         kernel_B_llp = kernel_B_llp / (8.0d0 * pi**3.0d0 * sol) * com_unit
             
     end subroutine

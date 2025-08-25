d51f4c85fb66a408d29db5d55a8a195151086d0f
Markus Markl <markl@tugraz.at>
2025-08-25 15:59:08 +0200

    feat: implement calculation of F0, F2 and H

--- KIM/src/asymptotics/FLR2_asymptotics.f90
+++ KIM/src/asymptotics/FLR2_asymptotics.f90
@@ -19,24 +19,27 @@ module flr2_asymptotics_m
         type(EBdat_t), intent(inout) :: EBdat
 
         integer :: sp
-        real(dp) :: F0, F2, H
+        complex(dp), allocatable :: F0(:), F2(:), H(:)
+
+        allocate(F0(size(EBdat%Br)))
+        allocate(F2(size(EBdat%Br)))
+        allocate(H(size(EBdat%Br)))
+
+        F0 = 0.0d0
+        H = 0.0d0
 
         do j = 1, size(EBdat%Br)
-            F0 = 0.0d0
-            H = 0.0d0
             do sp = 0, plasma_in%Nspec-1
-                F0 = F0 + 1.0d0/(plasma_in%spec(sp)%lambda_D(j)**2.0d0)
-                H = H + plasma%spec(sp)%rho_L(j)**2.0d0 / (plasma_in%spec(sp)%lambda_D(j)**2.0d0) &
+                F0(j) = F0(j) + 1.0d0/(plasma_in%spec(sp)%lambda_D(j)**2.0d0)
+                H(j) = H(j) + plasma%spec(sp)%rho_L(j)**2.0d0 / (plasma_in%spec(sp)%lambda_D(j)**2.0d0) &
                     * (1.0d0 - (plasma_in%spec(sp)%dndr(j) * plasma_in%spec(sp)%T(j)&
                         + plasma_in%spec(sp)%n(j) * plasma_in%spec(sp)%dTdr(j)) / &
                         (plasma_in%spec(sp)%Zspec * e_charge * plasma_in%spec(sp)%n(j) &
                         * plasma_in%Er(j)) )
             end do
-                F2 = F2
-                
-            end do
-            F0 = F0 * (- com_unit) * (sol * plasma_in%om_E(j) * plasma_in%Er(j) / (4.0d0 * pi *plasma_in%kp(j)**2.0d0))
-            H = H / (8.0d0 * pi)
+            F2(j) = - com_unit * plasma_in%om_E(j) * plasma_in%Er(j) / plasma_in%kp(j)**2.0d0 * H(j)
+            F0(j) = F0(j) * (- com_unit) * (sol * plasma_in%om_E(j) * plasma_in%Er(j) / (4.0d0 * pi *plasma_in%kp(j)**2.0d0))
+            H(j) = H(j) / (8.0d0 * pi)
         end do
 
     end subroutine

5a319fe14b75db8be4a8d48a383c197c15f2feca
Andreas Zach <andreas.zach@student.tugraz.at>
2025-09-03 23:09:39 +0200

    But appending is more efficient

--- QL-Balance/src/base/toroidal_torque.f90
+++ QL-Balance/src/base/toroidal_torque.f90
@@ -25,27 +25,27 @@ end subroutine calculate_total_toroidal_torque
 
 subroutine write_total_toroidal_torque_to_file(time_index)
     use grid_mod, only: T_tot_phi_e, T_tot_phi_i
-    use h5mod
+    use KAMEL_hdf5_tools, only: HID_T, H5T_NATIVE_DOUBLE, h5_init, h5_deinit, h5_open_rw, &
+                                h5_close, h5_define_unlimited_array, h5_append_double_0
+    use h5mod, only: h5_id, path2out, h5_mode_groupname, h5_currentgrp
 
     implicit none
 
     integer, intent(in) :: time_index
+    integer(HID_T), save :: dsetid_e, dsetid_i ! save == static
 
     call h5_init()
     call h5_open_rw(path2out, h5_id)
 
-    h5overwrite = .true.
+    if (time_index == 1) then
+        h5_currentgrp = "/"//trim(h5_mode_groupname)//"/T_tot_phi_e"
+        call h5_define_unlimited_array(h5_id, trim(h5_currentgrp), H5T_NATIVE_DOUBLE, dsetid_e)
+        h5_currentgrp = "/"//trim(h5_mode_groupname)//"/T_tot_phi_i"
+        call h5_define_unlimited_array(h5_id, trim(h5_currentgrp), H5T_NATIVE_DOUBLE, dsetid_i)
+    end if
 
-    h5_currentgrp = "/"//trim(h5_mode_groupname)//"/T_tot_phi_e"
-    call h5_add_double_1(h5_id, trim(h5_currentgrp), T_tot_phi_e(1:time_index), &
-                         lbound(T_tot_phi_e(1:time_index)), ubound(T_tot_phi_e(1:time_index)), &
-                         comment="Total toroidal torque on electrons")
-    h5_currentgrp = "/"//trim(h5_mode_groupname)//"/T_tot_phi_i"
-    call h5_add_double_1(h5_id, trim(h5_currentgrp), T_tot_phi_i(1:time_index), &
-                         lbound(T_tot_phi_i(1:time_index)), ubound(T_tot_phi_i(1:time_index)), &
-                         comment="Total toroidal torque on ions")
-
-    h5overwrite = .false.
+    call h5_append_double_0(dsetid_e, T_tot_phi_e(time_index), time_index)
+    call h5_append_double_0(dsetid_i, T_tot_phi_i(time_index), time_index)
 
     call h5_close(h5_id)
     call h5_deinit()

198b87334ced23f3534f3369d69438e7c13aba58
Andreas Zach <andreas.zach@student.tugraz.at>
2025-10-21 12:45:34 +0200

    Initialize NEO-RT in QL-Balance's init

--- QL-Balance/src/base/time_evolution_ntv.f90
+++ QL-Balance/src/base/time_evolution_ntv.f90
@@ -15,10 +15,26 @@ module time_evolution_ntv
 contains
 
     subroutine initTimeEvolutionNTV(this)
+        use do_magfie_mod, only: R0, s, bfac, do_magfie_init
+        use neort_profiles, only: init_profiles
+        use logger, only: set_log_level
+        use neort, only: read_and_set_control, read_and_init_plasma_input, &
+                         read_and_init_profile_input, init
+        use driftorbit, only: efac
+
         class(TimeEvolutionNTV_t), intent(inout) :: this
 
         call this%TimeEvolution_t%init_balance
         this%runType = "TimeEvolutionNTV"
+
+        ! NEO-RT
+        call read_and_set_control("neo-rt/driftorbit")
+        call do_magfie_init("neo-rt/in_file")
+        call init_profiles(R0)
+        call read_and_init_plasma_input("neo-rt/plasma.in", s)
+        call read_and_init_profile_input("neo-rt/profile.in", s, R0, efac, bfac)
+
+        call init
     end subroutine initTimeEvolutionNTV
 
     subroutine runTimeEvolutionNTV(this)

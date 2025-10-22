3465b07c38d9beac55f53b92b5b0ee5f797694d6
Andreas Zach <andreas.zach@student.tugraz.at>
2025-10-22 17:52:32 +0200

    Only initialization, handle everything via files the old way

--- QL-Balance/src/base/time_evolution_ntv.f90
+++ QL-Balance/src/base/time_evolution_ntv.f90
@@ -4,9 +4,9 @@ module time_evolution_ntv
     implicit none
 
     type, public, extends(TimeEvolution_t) :: TimeEvolutionNTV_t
-        contains
-            procedure :: init_balance => initTimeEvolutionNTV
-            procedure :: run_balance => runTimeEvolutionNTV
+    contains
+        procedure :: init_balance => initTimeEvolutionNTV
+        procedure :: run_balance => runTimeEvolutionNTV
     end type
 
     private :: initTimeEvolutionNTV
@@ -16,30 +16,52 @@ contains
 
     subroutine initTimeEvolutionNTV(this)
         use do_magfie_mod, only: R0, s, bfac, do_magfie_init
+        use do_magfie_pert_mod, only: do_magfie_pert_init
         use neort_profiles, only: init_profiles
         use logger, only: set_log_level
         use neort, only: read_and_set_control, read_and_init_plasma_input, &
-                         read_and_init_profile_input, init
+                         read_and_init_profile_input, init, check_magfie
         use driftorbit, only: efac
 
         class(TimeEvolutionNTV_t), intent(inout) :: this
 
-        call this%TimeEvolution_t%init_balance
-        this%runType = "TimeEvolutionNTV"
+        call this % TimeEvolution_t % init_balance
+        this % runType = "TimeEvolutionNTV"
 
         ! NEO-RT
-        call read_and_set_control("neo-rt/driftorbit")
-        call do_magfie_init("neo-rt/in_file")
-        call init_profiles(R0)
+        call set_log_level(4)  ! for development purposes
+
+        call read_and_set_control("neo-rt/driftorbit") ! NEO-RT config
+        call do_magfie_init("neo-rt/in_file") ! Boozer field file
+        ! call do_magfie_pert_init("neo-rt/in_file_pert") ! Boozer perturbed field file
+        call init_profiles(R0) ! minor stuff
         call read_and_init_plasma_input("neo-rt/plasma.in", s)
         call read_and_init_profile_input("neo-rt/profile.in", s, R0, efac, bfac)
 
         call init
+        call check_magfie("neo-rt/qlb")
     end subroutine initTimeEvolutionNTV
 
     subroutine runTimeEvolutionNTV(this)
+        use time_evolution, only: time_ind, Nstorage, doStep
+        use neort_profiles, only: init_thermodynamic_forces
+
         class(TimeEvolutionNTV_t), intent(inout) :: this
 
-        call this%TimeEvolution_t%run_balance()
+        do time_ind = 1, Nstorage
+            call doStep(this % TimeEvolution_t)
+            ! TODO: prepare data for NEO-RT
+            ! TODO: call NEO-RT
+
+            ! update NEO-RT inputs
+            ! call init_thermodynamic_forces(psi_pr, q) ! from subroutine init
+            ! call init_plasma_input(s, nplasma, am1, am2, Z1, Z2, plasma)
+            ! call init_profile_input(s, R0, efac, bfac, data)
+
+            ! call NEO-RT to compute transport
+            ! call compute_transport("neo-rt/qlb")
+        end do
+
     end subroutine runTimeEvolutionNTV
+
 end module

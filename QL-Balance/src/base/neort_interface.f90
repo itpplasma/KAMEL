2c8abf93939de61170c678cd118fd3c1bcb2cff9
Andreas Zach <andreas.zach@student.tugraz.at>
2025-11-03 20:18:03 +0100

    First part of major overhaul

--- QL-Balance/src/base/neort_interface.f90
+++ QL-Balance/src/base/neort_interface.f90
@@ -16,105 +16,92 @@ contains
 
     !> @brief Prepare plasma profile data for NEO-RT from KAMEL arrays
     !> @param[out] plasma_data 2D array (nflux, 6) for NEO-RT plasma input
-    subroutine prepare_plasma_data_for_neort(plasma_data)
-        use grid_mod, only: npoic, rc, Sc
+    subroutine prepare_plasma_data_for_neort(plasma_data, s_tor)
         use plasma_parameters, only: params
 
-        real(dp), intent(out) :: plasma_data(:, :)
+        real(dp), dimension(:, :), intent(out) :: plasma_data
+        real(dp), dimension(:), intent(in) :: s_tor
 
-        integer :: ipoi
-        real(dp), allocatable :: s(:)
+        integer :: i, s_size
+
+        s_size = size(s_tor)
 
         ! Check dimensions
-        if (size(plasma_data, 1) /= npoic) then
+        if (size(plasma_data, 1) /= s_size) then
             error stop "prepare_neort_plasma_data: plasma_data dimension mismatch"
         end if
         if (size(plasma_data, 2) /= 6) then
             error stop "prepare_neort_plasma_data: plasma_data must have six columns"
         end if
 
-        allocate(s(npoic))
-        call calculate_s(s, rc)
-
         ! Fill plasma data array
         ! c.f. e.g.: QL-Balance/src/base/paramscan.f90:257 and plasma.in file in NEO-RT's example base
-        do ipoi = 1, npoic
+        do i = 1, s_size
             ! Column 1: Normalized toroidal flux s (0 to 1)
-            plasma_data(ipoi, 1) = s(ipoi)
+            plasma_data(i, 1) = s_tor(i)
 
             ! Column 2: Density of species 1
-            plasma_data(ipoi, 2) = params(1, ipoi)
+            plasma_data(i, 2) = params(1, i)
 
             ! Column 3: Density of species 2
-            plasma_data(ipoi, 3) = 0d0
+            plasma_data(i, 3) = 0d0
 
             ! Column 4: Temperature of species 1
-            plasma_data(ipoi, 4) = params(4, ipoi)
+            plasma_data(i, 4) = params(4, i)
 
             ! Column 5: Temperature of species 2
-            plasma_data(ipoi, 5) = 1d0
+            plasma_data(i, 5) = 1d0
 
             ! Column 6: Electron temperature
-            plasma_data(ipoi, 6) = params(3, ipoi)
+            plasma_data(i, 6) = params(3, i)
         end do
 
     end subroutine prepare_plasma_data_for_neort
 
     !> @brief Prepare rotation profile data for NEO-RT from KAMEL arrays
     !> @param[out] profile_data 2D array (nflux, 3) for NEO-RT profile input
-    subroutine prepare_profile_data_for_neort(profile_data)
-        use grid_mod, only: npoic, rc, Ercov
-        use plasma_parameters, only: params, qsafb
+    subroutine prepare_profile_data_for_neort(profile_data, s_tor)
         use baseparam_mod, only: R0 => rtor, am, p_mass, btor
+        use grid_mod, only: Ercov
+        use plasma_parameters, only: params, qsafb
 
-        real(dp), intent(out) :: profile_data(:, :)
+        real(dp), dimension(:, :), intent(out) :: profile_data
+        real(dp), dimension(:), intent(in) :: s_tor
 
-        integer :: ipoi
-        real(dp), allocatable :: s(:)
+        integer :: i, s_size
         real(dp) :: T_i, m_i, vth, M_t, E_r, B_theta, B_mag
 
+        s_size = size(s_tor)
+
         ! Check dimensions
-        if (size(profile_data, 1) /= npoic) then
+        if (size(profile_data, 1) /= s_size) then
             error stop "prepare_neort_profile_data: profile_data dimension mismatch"
         end if
         if (size(profile_data, 2) /= 2) then
             error stop "prepare_neort_profile_data: profile_data must have two columns (s, M_t)"
         end if
 
-        allocate(s(npoic))
-        call calculate_s(s, rc)
-
         ! Fill profile data array
-        do ipoi = 1, npoic
+        ! TODO: check this, is is probably wrong
+        do i = 1, s_size
             ! Column 1: Normalized toroidal flux s (0 to 1)
-            profile_data(ipoi, 1) = s(ipoi)
+            profile_data(i, 1) = s_tor(i)
 
             ! Calculate thermal velocity and ion mass
-            T_i = params(4, ipoi)
+            T_i = params(4, i)
             m_i = am * p_mass  ! ion mass
             vth = sqrt(T_i / m_i)  ! thermal velocity
 
             ! Calculate ExB Mach number: M_t = E_r * B_theta / (B^2 * v_th)
-            E_r = Ercov(ipoi)
-            B_theta = btor / qsafb(ipoi)
+            E_r = Ercov(i)
+            B_theta = btor / qsafb(i)
             B_mag = sqrt(btor**2 + B_theta**2)
             M_t = E_r * B_theta / (B_mag**2 * vth)
 
             ! Column 2: ExB Mach number M_t
-            profile_data(ipoi, 2) = M_t
+            profile_data(i, 2) = M_t
         end do
 
     end subroutine prepare_profile_data_for_neort
-
-    subroutine calculate_s(s, r)
-        real(dp), intent(out) :: s(:)
-        real(dp), intent(in) :: r(:)
-
-        real(dp) :: rmin, rmax
-
-        rmin = minval(r)
-        rmax = maxval(r)
-        s = (r - rmin) / (rmax - rmin)
-    end subroutine calculate_s
 
 end module neort_interface
--- QL-Balance/src/base/time_evolution_ntv.f90
+++ QL-Balance/src/base/time_evolution_ntv.f90
@@ -13,94 +13,126 @@ module time_evolution_ntv
 
     private :: initTimeEvolutionNTV
     private :: runTimeEvolutionNTV
+    private :: doStep
 
     ! for NEO-RT
-    real(dp), allocatable :: plasma_data(:, :)
-    real(dp), allocatable :: profile_data(:, :)
-    real(dp) :: am1, am2, Z1, Z2, s, efac, bfac
+    real(dp), dimension(:), allocatable :: s_tor
+    real(dp), dimension(:, :), allocatable :: plasma_data
+    real(dp), dimension(:, :), allocatable :: profile_data
+    real(dp) :: am1, am2, Z1, Z2, efac, bfac
 
     ! from NEO-RT
     type(magfie_data_t) :: magfie_data
-    type(transport_data_t), allocatable :: transport_data(:)
+    type(transport_data_t), dimension(:), allocatable :: transport_data
 
 contains
 
     subroutine initTimeEvolutionNTV(this)
-        use do_magfie_mod, only: do_magfie_init
-        use do_magfie_pert_mod, only: do_magfie_pert_init
         use grid_mod, only: npoic
         use logger, only: set_log_level
-        use neort, only: read_and_set_control, init, check_magfie
-        use neort_interface, only: prepare_plasma_data_for_neort, prepare_profile_data_for_neort
-        use neort_profiles, only: init_profiles, init_plasma_input, init_profile_input
-        use baseparam_mod, only: R0 => rtor, am, Z_i
 
         class(TimeEvolutionNTV_t), intent(inout) :: this
+        real(dp), dimension(:), allocatable :: phi_tor
 
         call this%TimeEvolution_t%init_balance
         this%runType = "TimeEvolutionNTV"
 
         ! NEO-RT
-        allocate (plasma_data(npoic, 6))
-        allocate (profile_data(npoic, 2))
+        ! allocate (phi_tor(npoic))
+        ! allocate (s_tor(npoic))
+        ! allocate (plasma_data(npoic, 6))
+        ! allocate (profile_data(npoic, 2))
+        ! allocate (transport_data(npoic))
 
-        ! set parameters
-        s = 0.5d0
-        efac = 1d0
-        bfac = 1d0
-        ! pass same mass and charge for species 1 and 2
-        am1 = am
-        am2 = am
-        Z1 = Z_i
-        Z2 = Z_i
+        ! call read_toroidal_flux(phi_tor)
+        ! call calculate_s_tor(s_tor, phi_tor)
+        ! ONLY FOR NOW, DEBUG!
+        allocate (phi_tor(dim_for_now))
+        allocate (s_tor(dim_for_now))
+        allocate (plasma_data(dim_for_now, 6))
+        allocate (profile_data(dim_for_now, 2))
+        allocate (transport_data(dim_for_now))
+        s_tor(1) = 0.3d0
+        s_tor(2) = 0.4d0
+        s_tor(3) = 0.5d0
+        s_tor(4) = 0.6d0
+        s_tor(5) = 0.7d0
+
+        ! ! set parameters
+        ! s = 0.5d0
+        ! efac = 1d0
+        ! bfac = 1d0
+        ! ! pass same mass and charge for species 1 and 2
+        ! am1 = am
+        ! am2 = am
+        ! Z1 = Z_i
+        ! Z2 = Z_i
 
         call set_log_level(4)  ! for development purposes
 
-        call read_and_set_control("neo-rt/driftorbit") ! NEO-RT config
-        call do_magfie_init("neo-rt/in_file") ! Boozer field file
-        ! call do_magfie_pert_init("neo-rt/in_file_pert") ! Boozer perturbed field file
-        call init_profiles(R0)
+        ! ! TODO: loop over
 
-        call prepare_plasma_data_for_neort(plasma_data)
-        call prepare_profile_data_for_neort(profile_data)
+        ! call read_and_set_control("neo-rt/driftorbit")  ! NEO-RT config, TODO: can be done without file
+        ! ! TODO: make sure that NEO-RT does not use globals that are not set without this call!
+        ! call do_magfie_init("neo-rt/in_file")  ! Boozer field file !! CAVEAT: sets other stuff as well (like R0) , must be kept with file, no other way around
+        ! ! call do_magfie_pert_init("neo-rt/in_file_pert") ! Boozer perturbed field file ! Maybe TODO:
+        ! call init_profiles(R0)
 
-        call init_plasma_input(s, npoic, am1, am2, Z1, Z2, plasma_data)
-        call init_profile_input(s, R0, efac, bfac, profile_data)
+        ! call prepare_plasma_data_for_neort(plasma_data)
+        ! call prepare_profile_data_for_neort(profile_data)
 
-        call init
-        call check_magfie(magfie_data)
+        ! ! calculates only on a fixed s!! => loop over grid
+        ! call init_plasma_input(s, npoic, am1, am2, Z1, Z2, plasma_data)
+        ! call init_profile_input(s, R0, efac, bfac, profile_data)
+
+        ! call init()
     end subroutine initTimeEvolutionNTV
 
     subroutine runTimeEvolutionNTV(this)
-        use baseparam_mod, only: R0 => rtor, btor
-        use grid_mod, only: npoic, rc
-        use neort, only: compute_transport
-        use neort_interface, only: prepare_plasma_data_for_neort, prepare_profile_data_for_neort
-        use neort_profiles, only: init_profiles, init_plasma_input, init_profile_input, &
-                                  init_thermodynamic_forces
-        use plasma_parameters, only: qsafb
-        use time_evolution, only: time_ind, Nstorage, doStep
+        use time_evolution, only: time_ind, Nstorage
 
         class(TimeEvolutionNTV_t), intent(inout) :: this
 
-        real(dp) :: q, r, psi_pr
-
-        allocate (transport_data(Nstorage))
-
-        q = sum(qsafb) / size(qsafb)
-        r = sum(rc) / size(rc)
-        psi_pr = r * btor / q
-
         do time_ind = 1, Nstorage
-            call doStep(this%TimeEvolution_t)
+            call doStep(this)
+        end do
+    end subroutine runTimeEvolutionNTV
 
-            call prepare_plasma_data_for_neort(plasma_data)
-            call prepare_profile_data_for_neort(profile_data)
+    subroutine doStep(this)
+        use baseparam_mod, only: R0 => rtor, btor
+        use do_magfie_mod, only: do_magfie_init
+        use do_magfie_pert_mod, only: do_magfie_pert_init
+        use neort, only: read_and_set_control, init, compute_transport
+        use neort_interface, only: prepare_plasma_data_for_neort, prepare_profile_data_for_neort
+        use neort_profiles, only: init_profiles, init_plasma_input, init_profile_input
+        use time_evolution, only: time_ind, doStepBase => doStep
 
+        class(TimeEvolutionNTV_t), intent(inout) :: this
+
+        integer :: s_idx
+        integer :: s_size
+
+        call doStepBase(this%TimeEvolution_t)
+
+        ! NEO-RT
+        s_size = size(s_tor)
+        do s_idx = 1, s_size
+            call read_and_set_control("neo-rt/driftorbit")  ! NEO-RT config, TODO: can be done without file
+            ! TODO: make sure that NEO-RT does not use globals that are not set without this call!
+            call do_magfie_init("neo-rt/in_file")  ! Boozer field file !! CAVEAT: sets other stuff as well (like R0) , must be kept with file, no other way around
+            ! call do_magfie_pert_init("neo-rt/in_file_pert") ! Boozer perturbed field file ! Maybe TODO:
             call init_profiles(R0)
-            call init_plasma_input(s, npoic, am1, am2, Z1, Z2, plasma_data)
-            call init_profile_input(s, R0, efac, bfac, profile_data)
-            call init_thermodynamic_forces(psi_pr, q)
+
+            call prepare_plasma_data_for_neort(plasma_data, s_tor)
+            call prepare_profile_data_for_neort(profile_data, s_tor)
+
+            call init_plasma_input(s_tor(s_idx), s_size, am1, am2, Z1, Z2, plasma_data)
+            call init_profile_input(s_tor(s_idx), R0, efac, bfac, profile_data)
+
+            call init()
+
+            ! TODO: omp loop over all s, do init and compute_transport for each s here
+            ! call init_thermodynamic_forces(psi_pr, q)
 
             call compute_transport(transport_data(time_ind))
 
@@ -109,6 +141,18 @@ contains
             ! For example:
             ! call apply_ntv_transport(D11_ntv, D12_ntv, torque_ntv)
         end do
-    end subroutine runTimeEvolutionNTV
+    end subroutine doStep
+
+
+    subroutine calculate_s_tor(s, phi)
+        real(dp), dimension(:), intent(out) :: s
+        real(dp), dimension(:), intent(in) :: phi
+
+        real(dp) :: phi_min, phi_max
+
+        phi_min = phi(1)
+        phi_max = phi(size(phi))
+        s = (phi - phi_min) / (phi_max - phi_min)
+    end subroutine calculate_s_tor
 
 end module

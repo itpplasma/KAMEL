f574407c9e9365a0af6e90345d0ed77b0cc3e945
marjohma <73427010+marjohma@users.noreply.github.com>
2026-08-14 10:42:13 +0200

    test(KIM): optimize radial-current harmonic scan

--- KIM/tests/test_radial_current_harmonics.f90
+++ KIM/tests/test_radial_current_harmonics.f90
@@ -11,7 +11,9 @@ program test_radial_current_harmonics
 contains
 
     subroutine run_harmonic_study()
-        use config_m, only: nml_config_path, profiles_in_memory
+        use config_m, only: nml_config_path, periodic_dr_asis_scale, &
+            periodic_dr_tr_scale, periodic_kmax_scale, periodic_n_rg, profiles_in_memory
+        use constants_m, only: pi
         use grid_m, only: rg_grid
         use kim_base_m, only: kim_t
         use kim_mod_m, only: from_kim_factory_get_kim
@@ -24,31 +26,64 @@ contains
         use species_m, only: plasma, set_profiles_from_arrays
 
         integer, parameter :: max_cutoff = 6
-        integer, parameter :: fourier_cutoff = 3
         integer, parameter :: n_profile = 201
-        integer, parameter :: n_window = 64
-        integer, parameter :: dim = 2 * fourier_cutoff + 1
         real(dp) :: r(n_profile), density(n_profile), te(n_profile), ti(n_profile)
         real(dp) :: q(n_profile), er(n_profile)
         class(kim_t), allocatable :: kim
         complex(dp), allocatable :: Kphi(:,:), KB(:,:), Kjphi(:,:), KjB(:,:)
         complex(dp), allocatable :: unused_jrphi(:,:), unused_jrB(:,:), phi_m(:)
-        complex(dp) :: Kjrphi(dim, dim, 0:max_cutoff)
-        complex(dp) :: KjrB(dim, dim, 0:max_cutoff)
-        complex(dp) :: KjrBparallel(dim, dim, 0:max_cutoff)
-        complex(dp) :: jrad(n_window, 0:max_cutoff)
+        complex(dp), allocatable :: Kjrphi(:,:,:), KjrB(:,:,:), KjrBparallel(:,:,:)
+        complex(dp), allocatable :: jrad(:,:), jrad_bparallel(:,:), jrad_total(:,:)
         complex(dp) :: sample(3, 0:max_cutoff)
-        real(dp) :: rm, rho_l_rm, dx_asis, dx_tr, period
+        real(dp) :: rm, rho_l_rm, dx_asis, dx_tr, period, dr_asis_scale, dr_tr_scale
         real(dp) :: full_norm(3), shell_rel(3), sample_rel(3)
         real(dp) :: jrad_full_norm, jrad_shell_rel, jrad_tail_rel
-        integer :: cutoff, info, j, nearest
+        character(1024) :: external_config, profile_output
+        integer :: argument_count, cutoff, dim, fourier_cutoff, info, j, nearest
+        integer :: n_window, path_length, status
+        logical :: external_run, write_profiles
 
-        call make_test_profiles(r, density, te, ti, q, er)
-        call write_test_namelist('KIM_config_radial_harmonics_test.nml')
-        nml_config_path = 'KIM_config_radial_harmonics_test.nml'
-        profiles_in_memory = .true.
+        argument_count = command_argument_count()
+        if (argument_count > 1) then
+            error stop 'usage: test_radial_current_harmonics.x [config.nml]'
+        end if
+        external_run = argument_count >= 1
+        write_profiles = external_run
+        if (external_run) then
+            call get_command_argument(1, external_config, length=path_length, status=status)
+            if (status /= 0 .or. path_length <= 0) error stop 'invalid external config path'
+            nml_config_path = external_config(:path_length)
+            profiles_in_memory = .false.
+        else
+            call make_test_profiles(r, density, te, ti, q, er)
+            call write_test_namelist('KIM_config_radial_harmonics_test.nml')
+            nml_config_path = 'KIM_config_radial_harmonics_test.nml'
+            profiles_in_memory = .true.
+        end if
+        if (external_run) then
+            call get_environment_variable('KIM_RADIAL_PROFILE_OUTPUT', profile_output, &
+                length=path_length, status=status)
+            if (status /= 0 .or. path_length <= 0) then
+                error stop 'external study requires KIM_RADIAL_PROFILE_OUTPUT'
+            end if
+            profile_output = profile_output(:path_length)
+        else
+            profile_output = ''
+        end if
         call kim_init()
-        call set_profiles_from_arrays(r, density, te, ti, q, er, n_profile)
+        if (external_run) then
+            dr_asis_scale = periodic_dr_asis_scale
+            dr_tr_scale = periodic_dr_tr_scale
+            fourier_cutoff = ceiling(periodic_kmax_scale &
+                * (dr_asis_scale + dr_tr_scale) / pi)
+            n_window = periodic_n_rg
+        else
+            dr_asis_scale = 5.0_dp
+            dr_tr_scale = 10.0_dp
+            fourier_cutoff = 3
+            n_window = 64
+            call set_profiles_from_arrays(r, density, te, ti, q, er, n_profile)
+        end if
         call from_kim_factory_get_kim('electrostatic', kim)
         call kim%init()
 
@@ -57,10 +92,15 @@ contains
         rm = r_res
         nearest = minloc(abs(plasma%r_grid - rm), dim=1)
         rho_l_rm = plasma%spec(1)%rho_L(nearest)
-        dx_asis = 5.0_dp * rho_l_rm
-        dx_tr = 10.0_dp * rho_l_rm
+        dx_asis = dr_asis_scale * rho_l_rm
+        dx_tr = dr_tr_scale * rho_l_rm
         period = 2.0_dp * (dx_asis + dx_tr)
         call build_periodic_plasma(rm, dx_asis, dx_tr, n_window)
+        dim = 2 * fourier_cutoff + 1
+        allocate(Kjrphi(dim, dim, 0:max_cutoff), KjrB(dim, dim, 0:max_cutoff), &
+            KjrBparallel(dim, dim, 0:max_cutoff), jrad(n_window, 0:max_cutoff), &
+            jrad_bparallel(n_window, 0:max_cutoff), &
+            jrad_total(n_window, 0:max_cutoff))
 
         call assemble_periodic_matrices(plasma, period, fourier_cutoff, Kphi, KB, &
             Kjphi, KjB, unused_jrphi, unused_jrB)
@@ -69,12 +109,17 @@ contains
         if (info /= 0) error stop 'radial harmonic study periodic solve failed'
 
         j = rg_grid%npts_b / 2
+        call assemble_radial_matrix_cutoffs(period, fourier_cutoff, max_cutoff, &
+            Kjrphi, KjrB, KjrBparallel)
         do cutoff = 0, max_cutoff
-            call assemble_radial_matrices(period, fourier_cutoff, cutoff, &
-                Kjrphi(:, :, cutoff), KjrB(:, :, cutoff), KjrBparallel(:, :, cutoff))
             jrad(:, cutoff) = reconstruct_jrad(Kjrphi(:, :, cutoff), &
                 KjrB(:, :, cutoff), phi_m, (1.0_dp, 0.0_dp), period, &
                 fourier_cutoff, rg_grid%xb)
+            jrad_total(:, cutoff) = reconstruct_jrad(Kjrphi(:, :, cutoff), &
+                KjrB(:, :, cutoff), phi_m, (1.0_dp, 0.0_dp), period, &
+                fourier_cutoff, rg_grid%xb, KjrBparallel(:, :, cutoff), &
+                (1.0_dp, 0.0_dp))
+            jrad_bparallel(:, cutoff) = jrad_total(:, cutoff) - jrad(:, cutoff)
             sample(1, cutoff) = hatG_jrad_phi(plasma, 2.0_dp / period, &
                 -1.0_dp / period, j, cutoff)
             sample(2, cutoff) = hatG_jrad_br(plasma, 2.0_dp / period, &
@@ -87,8 +132,11 @@ contains
         call assert_finite_3d(KjrB, 'KjrBr')
         call assert_finite_3d(KjrBparallel, 'KjrBparallel')
         call assert_finite_2d(jrad, 'jrad')
+        call assert_finite_2d(jrad_bparallel, 'jrad_Bparallel')
+        call assert_finite_2d(jrad_total, 'jrad_total')
         call assert_finite_2d(sample, 'fixed-sample kernels')
-        call maybe_write_jrad_profiles(rg_grid%xb, jrad)
+        call maybe_write_jrad_profiles(rg_grid%xb, jrad, jrad_bparallel, &
+            jrad_total, trim(profile_output), write_profiles)
 
         full_norm = [frobenius_norm(Kjrphi(:, :, max_cutoff)), &
             frobenius_norm(KjrB(:, :, max_cutoff)), &
@@ -141,22 +189,31 @@ contains
         end if
     end subroutine run_harmonic_study
 
-    subroutine assemble_radial_matrices(period, M, cutoff, Kjrphi, KjrB, KjrBparallel)
+    subroutine assemble_radial_matrix_cutoffs(period, M, max_cutoff, Kjrphi, KjrB, &
+            KjrBparallel)
         use constants_m, only: pi
         use grid_m, only: rg_grid
         use periodic_assembly_m, only: k_of_m
-        use radial_current_fourier_kernel_m, only: hatG_jrad_bparallel, &
-            hatG_jrad_br, hatG_jrad_phi
+        use radial_current_fourier_kernel_m, only: hatG_jrad_bparallel_harmonic, &
+            hatG_jrad_br_harmonic, hatG_jrad_phi_harmonic
         use species_m, only: plasma
 
         real(dp), intent(in) :: period
-        integer, intent(in) :: M, cutoff
-        complex(dp), intent(out) :: Kjrphi(:, :), KjrB(:, :), KjrBparallel(:, :)
-        complex(dp) :: acc_phi, acc_B, acc_Bparallel
+        integer, intent(in) :: M, max_cutoff
+        complex(dp), intent(out) :: Kjrphi(:, :, 0:), KjrB(:, :, 0:)
+        complex(dp), intent(out) :: KjrBparallel(:, :, 0:)
+        complex(dp) :: acc_phi(-max_cutoff:max_cutoff)
+        complex(dp) :: acc_B(-max_cutoff:max_cutoff)
+        complex(dp) :: acc_Bparallel(-max_cutoff:max_cutoff)
         real(dp) :: kr_response, kr_source, weight
-        integer :: im, imp, j, m_response, m_source
+        integer :: cutoff, ell, im, imp, j, m_response, m_source
 
         weight = 2.0_dp * pi / real(rg_grid%npts_b, dp)
+        !$omp parallel do default(none) schedule(static) &
+        !$omp shared(M, max_cutoff, period, weight, plasma, rg_grid, Kjrphi, KjrB, &
+        !$omp        KjrBparallel) &
+        !$omp private(m_source, imp, kr_source, m_response, im, kr_response, j, ell, &
+        !$omp         cutoff, acc_phi, acc_B, acc_Bparallel)
         do m_source = -M, M
             imp = m_source + M + 1
             kr_source = k_of_m(m_source, period)
@@ -167,19 +224,31 @@ contains
                 acc_B = (0.0_dp, 0.0_dp)
                 acc_Bparallel = (0.0_dp, 0.0_dp)
                 do j = 1, rg_grid%npts_b
-                    acc_phi = acc_phi + hatG_jrad_phi(plasma, kr_response, kr_source, &
-                        j, cutoff)
-                    acc_B = acc_B + hatG_jrad_br(plasma, kr_response, kr_source, &
-                        j, cutoff)
-                    acc_Bparallel = acc_Bparallel + hatG_jrad_bparallel(plasma, &
-                        kr_response, kr_source, j, cutoff)
+                    do ell = -max_cutoff, max_cutoff
+                        acc_phi(ell) = acc_phi(ell) + hatG_jrad_phi_harmonic(plasma, &
+                            ell, kr_response, kr_source, j)
+                        acc_B(ell) = acc_B(ell) + hatG_jrad_br_harmonic(plasma, &
+                            ell, kr_response, kr_source, j)
+                        acc_Bparallel(ell) = acc_Bparallel(ell) &
+                            + hatG_jrad_bparallel_harmonic(plasma, ell, kr_response, &
+                            kr_source, j)
+                    end do
                 end do
-                Kjrphi(im, imp) = weight * acc_phi
-                KjrB(im, imp) = weight * acc_B
-                KjrBparallel(im, imp) = weight * acc_Bparallel
+                Kjrphi(im, imp, 0) = weight * acc_phi(0)
+                KjrB(im, imp, 0) = weight * acc_B(0)
+                KjrBparallel(im, imp, 0) = weight * acc_Bparallel(0)
+                do cutoff = 1, max_cutoff
+                    Kjrphi(im, imp, cutoff) = Kjrphi(im, imp, cutoff - 1) &
+                        + weight * (acc_phi(-cutoff) + acc_phi(cutoff))
+                    KjrB(im, imp, cutoff) = KjrB(im, imp, cutoff - 1) &
+                        + weight * (acc_B(-cutoff) + acc_B(cutoff))
+                    KjrBparallel(im, imp, cutoff) = KjrBparallel(im, imp, cutoff - 1) &
+                        + weight * (acc_Bparallel(-cutoff) + acc_Bparallel(cutoff))
+                end do
             end do
         end do
-    end subroutine assemble_radial_matrices
+        !$omp end parallel do
+    end subroutine assemble_radial_matrix_cutoffs
 
     real(dp) function frobenius_norm(matrix) result(norm)
         complex(dp), intent(in) :: matrix(:, :)
@@ -211,25 +280,26 @@ contains
             .not. all(ieee_is_finite(aimag(values)))) error stop label//' is non-finite'
     end subroutine assert_finite_3d
 
-    subroutine maybe_write_jrad_profiles(radii, jrad)
+    subroutine maybe_write_jrad_profiles(radii, jrad, jrad_bparallel, jrad_total, &
+            output_path, write_profiles)
         real(dp), intent(in) :: radii(:)
-        complex(dp), intent(in) :: jrad(:, 0:)
-        character(1024) :: output_path
-        integer :: cutoff, i, path_length, status, unit
+        complex(dp), intent(in) :: jrad(:, 0:), jrad_bparallel(:, 0:)
+        complex(dp), intent(in) :: jrad_total(:, 0:)
+        character(*), intent(in) :: output_path
+        logical, intent(in) :: write_profiles
+        integer :: cutoff, i, unit
 
-        call get_environment_variable('KIM_HARMONIC_PROFILE_PATH', output_path, &
-            length=path_length, status=status)
-        if (status == 1) return
-        if (status /= 0 .or. path_length <= 0) then
-            error stop 'invalid KIM_HARMONIC_PROFILE_PATH'
-        end if
+        if (.not. write_profiles) return
 
-        open(newunit=unit, file=output_path(:path_length), status='replace', &
-            action='write')
+        open(newunit=unit, file=output_path, status='replace', action='write')
         write(unit, '(A)', advance='no') 'radius_cm'
         do cutoff = lbound(jrad, 2), ubound(jrad, 2)
             write(unit, '(A,I0,A,I0)', advance='no') ',jrad_re_N', cutoff, &
                 ',jrad_im_N', cutoff
+            write(unit, '(A,I0,A,I0)', advance='no') ',jrad_bparallel_re_N', cutoff, &
+                ',jrad_bparallel_im_N', cutoff
+            write(unit, '(A,I0,A,I0)', advance='no') ',jrad_total_re_N', cutoff, &
+                ',jrad_total_im_N', cutoff
         end do
         write(unit, *)
 
@@ -238,6 +308,11 @@ contains
             do cutoff = lbound(jrad, 2), ubound(jrad, 2)
                 write(unit, '(A,ES24.16,A,ES24.16)', advance='no') ',', &
                     real(jrad(i, cutoff), dp), ',', aimag(jrad(i, cutoff))
+                write(unit, '(A,ES24.16,A,ES24.16)', advance='no') ',', &
+                    real(jrad_bparallel(i, cutoff), dp), ',', &
+                    aimag(jrad_bparallel(i, cutoff))
+                write(unit, '(A,ES24.16,A,ES24.16)', advance='no') ',', &
+                    real(jrad_total(i, cutoff), dp), ',', aimag(jrad_total(i, cutoff))
             end do
             write(unit, *)
         end do

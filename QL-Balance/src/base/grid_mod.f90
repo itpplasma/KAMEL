829e9a67dcce51e28908607751ead3c68fb17c89
Andreas Zach <andreas.zach@student.tugraz.at>
2026-08-15 13:01:09 +0200

    Write total ntv torque to file as well

--- QL-Balance/src/base/grid_mod.f90
+++ QL-Balance/src/base/grid_mod.f90
@@ -22,9 +22,8 @@ module grid_mod
     real(dp), dimension(:),   allocatable :: dae11,dae12,dae22
     real(dp), dimension(:),   allocatable :: dai11,dai12,dai22
     real(dp), dimension(:), allocatable :: dni22, visca, polforce
-    real(dp), dimension(:), allocatable :: T_EM_phi_e, T_EM_phi_i
-    real(dp), dimension(:), allocatable :: T_tot_phi_e, T_tot_phi_i
-    real(dp), dimension(:), allocatable :: torque_ntv
+    real(dp), dimension(:), allocatable :: T_EM_phi_e, T_EM_phi_i, torque_ntv
+    real(dp), dimension(:), allocatable :: T_tot_phi_e, T_tot_phi_i, T_tot_ntv_i
     ! ql RMP-induced diffusion
     real(dp), dimension(:),   allocatable :: dqle11,dqle12,dqle21,dqle22
     real(dp), dimension(:),   allocatable :: dqli11,dqli12,dqli21,dqli22
--- QL-Balance/src/base/toroidal_torque.f90
+++ QL-Balance/src/base/toroidal_torque.f90
@@ -1,7 +1,7 @@
 subroutine calculate_total_toroidal_torque(time_index)
     use iso_fortran_env, only: dp => real64
     use baseparam_mod, only: pi, rtor ! rtor is the major radius
-    use grid_mod, only: T_EM_phi_e, T_EM_phi_i, T_tot_phi_e, T_tot_phi_i
+    use grid_mod, only: T_EM_phi_e, T_EM_phi_i, torque_ntv, T_tot_phi_e, T_tot_phi_i, T_tot_ntv_i
     use simpson_integration, only: simpson_nonequi
     use wave_code_data, only: r
 
@@ -11,14 +11,16 @@ subroutine calculate_total_toroidal_torque(time_index)
 
     call simpson_nonequi(T_tot_phi_e(time_index), r, r * T_EM_phi_e)
     call simpson_nonequi(T_tot_phi_i(time_index), r, r * T_EM_phi_i)
+    call simpson_nonequi(T_tot_ntv_i(time_index), r, r * torque_ntv)
 
     T_tot_phi_e(time_index) = T_tot_phi_e(time_index) * 4.0_dp * pi**2 * rtor
     T_tot_phi_i(time_index) = T_tot_phi_i(time_index) * 4.0_dp * pi**2 * rtor
+    T_tot_ntv_i(time_index) = T_tot_ntv_i(time_index) * 4.0_dp * pi**2 * rtor
 
 end subroutine calculate_total_toroidal_torque
 
 subroutine write_total_toroidal_torque_to_file(time_index)
-    use grid_mod, only: T_tot_phi_e, T_tot_phi_i
+    use grid_mod, only: T_tot_phi_e, T_tot_phi_i, T_tot_ntv_i
     use KAMEL_hdf5_tools, only: HID_T, H5T_NATIVE_DOUBLE, h5_init, h5_deinit, h5_open_rw, &
                                 h5_close, h5_define_unlimited_array, h5_append_double_0
     use h5mod, only: h5_id, path2out, h5_mode_groupname, h5_currentgrp
@@ -26,7 +28,7 @@ subroutine write_total_toroidal_torque_to_file(time_index)
     implicit none
 
     integer, intent(in) :: time_index
-    integer(HID_T), save :: dsetid_e, dsetid_i ! save == static
+    integer(HID_T), save :: dsetid_e, dsetid_i, dsetid_ntv ! save == static
 
     call h5_init()
     call h5_open_rw(path2out, h5_id)
@@ -36,10 +38,13 @@ subroutine write_total_toroidal_torque_to_file(time_index)
         call h5_define_unlimited_array(h5_id, trim(h5_currentgrp), H5T_NATIVE_DOUBLE, dsetid_e)
         h5_currentgrp = "/"//trim(h5_mode_groupname)//"/T_tot_phi_i"
         call h5_define_unlimited_array(h5_id, trim(h5_currentgrp), H5T_NATIVE_DOUBLE, dsetid_i)
+        h5_currentgrp = "/"//trim(h5_mode_groupname)//"/T_tot_ntv_i"
+        call h5_define_unlimited_array(h5_id, trim(h5_currentgrp), H5T_NATIVE_DOUBLE, dsetid_ntv)
     end if
 
     call h5_append_double_0(dsetid_e, T_tot_phi_e(time_index), time_index)
     call h5_append_double_0(dsetid_i, T_tot_phi_i(time_index), time_index)
+    call h5_append_double_0(dsetid_ntv, T_tot_ntv_i(time_index), time_index)
 
     call h5_close(h5_id)
     call h5_deinit()
